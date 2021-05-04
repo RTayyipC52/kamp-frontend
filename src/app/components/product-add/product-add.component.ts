7cc86d82401472a4fdfef482a5fa26671a7e2108
RTayyipC52 <tayyipcicek52@gmail.com>
2021-05-04 23:28:03 +0300

    Ayar yapıldı

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -32,10 +32,9 @@ export class ProductAddComponent implements OnInit {
     if (this.productAddForm.valid) {
       let productModel = Object.assign({}, this.productAddForm.value)
       this.productService.add(productModel).subscribe(response=>{
-        
         this.toastrService.success(response.message, "Başarılı")
       },responseError=>{
-        if(responseError.error.ValidationErrors.lenght>0){
+        if(responseError.error.ValidationErrors.length>0){
           for (let i = 0; i < responseError.error.ValidationErrors.length; i++) {
             this.toastrService.error(responseError.error.ValidationErrors[i].ErrorMessage,"Doğrulama Hatası")
           }
